af15eb95c1069c6514d8bea7d0a493b5e265f903
ysl2 <songli.yu@outlook.com>
2021-01-31 21:33:49 +0800

    2021-01-31

--- .MY-AUTOLOAD-SERVICES/rc.local.d/auto-mount-disk.sh
+++ .MY-AUTOLOAD-SERVICES/rc.local.d/auto-mount-disk.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-sudo mkdir -p /home/yusoli/Public/sda1
-sudo mount /dev/sda1 /home/yusoli/Public/sda1
-sudo mkdir -p /home/yusoli/Public/sda5
-sudo mount /dev/sda5 /home/yusoli/Public/sda5
-sudo mkdir -p /home/yusoli/Public/sda6
-sudo mount /dev/sda6 /home/yusoli/Public/sda6
+
+# For real machine
+#sudo mkdir -p /home/yusoli/Public/sda1
+#sudo mount /dev/sda1 /home/yusoli/Public/sda1
+#sudo mkdir -p /home/yusoli/Public/sda5
+#sudo mount /dev/sda5 /home/yusoli/Public/sda5
+#sudo mkdir -p /home/yusoli/Public/sda6
+#sudo mount /dev/sda6 /home/yusoli/Public/sda6
+
+# For virtual machine
+sudo mkdir -p /media/cdrom
+sudo mount /dev/cdrom/media/cdrom
 exit 0
