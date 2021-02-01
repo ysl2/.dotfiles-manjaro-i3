7a1762e962eed8f9c41b5eb7d0f3c6280526e91d
ysl2 <songli.yu@outlook.com>
2021-02-01 08:49:21 +0800

    2021-02-01 08:49:21

--- .MY-GLOBAL-SETTINGS/restore-all.sh
+++ .MY-GLOBAL-SETTINGS/restore-all.sh
@@ -7,8 +7,8 @@ sudo ln -s /home/yusoli/.dotfiles/.MY-GLOBAL-SETTINGS/powerpill/powerpill.json /
 sudo rm /etc/hosts
 sudo ln -s /home/yusoli/.dotfiles/.MY-GLOBAL-SETTINGS/hosts /etc/hosts
 
-sudo rm /etc/makepkg.conf
-sudo ln -s /home/yusoli/.dotfiles/.MY-GLOBAL-SETTINGS/makepkg.conf /etc/makepkg.conf
+#sudo rm /etc/makepkg.conf
+#sudo ln -s /home/yusoli/.dotfiles/.MY-GLOBAL-SETTINGS/makepkg.conf /etc/makepkg.conf
 
 sudo rm /etc/pacman.d/mirrorlist
 sudo ln -s /home/yusoli/.dotfiles/.MY-GLOBAL-SETTINGS/pacman.d/mirrorlist /etc/pacman.d/mirrorlist
