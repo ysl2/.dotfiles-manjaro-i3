6d6a3af8f21c27ac4148cd975e4b0b78f280ef1e
ysl2 <songli.yu@outlook.com>
2021-02-07 21:28:28 +0800

    2021-02-07 21:28:28

--- my-package-upload.sh
+++ my-package-upload.sh
@@ -1,6 +1,6 @@
-mkdir -p .MY-PACKAGE-LIST
-mkdir -p .MY-PACKAGE-LIST/i3
-cd .MY-PACKAGE-LIST/i3
+mkdir -p /home/yusoli/.dotfiles/.MY-PACKAGE-LIST
+mkdir -p /home/yusoli/.dotfiles/.MY-PACKAGE-LIST/i3
+cd /home/yusoli/.dotfiles/.MY-PACKAGE-LIST/i3
 
 if [[ ! -f "my-pacman-list.txt" ]]; then
 	touch my-pacman-list.txt
@@ -11,3 +11,6 @@ fi
 sudo pacman -Qeq > my-pacman-list.txt
 yay -Qeq > my-yay-list.txt
 pip list | awk '{print $1}' > my-pip-list.txt
+
+cd /home/yusoli/.dotfiles/
+git add . > /dev/null && git commit -m "$(date +"%Y-%m-%d %H:%M:%S")" > /dev/null && git push origin master -q > /dev/null
