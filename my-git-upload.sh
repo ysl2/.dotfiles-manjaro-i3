38c986c14e227ed1278bc756f288f63161e0047e
ysl2 <songli.yu@outlook.com>
2021-02-08 08:40:16 +0800

    2021-02-08 08:40:16

--- my-git-upload.sh
+++ my-git-upload.sh
@@ -1,6 +1,12 @@
 # 用于上传dotfiles
 #----------------------------------------
 
+if [[ -e /home/yusoli/.dotfiles/ ]]; then
+	cd /home/yusoli/.dotfiles/
+	git pull --ff-only
+fi
+
+cd /home/yusoli/.dotfiles-manjaro-i3/
 git add . > /dev/null && git commit -m "$(date +"%Y-%m-%d %H:%M:%S")" > /dev/null && git push origin master -q > /dev/null
 
 #sudo powerpill -Sy
