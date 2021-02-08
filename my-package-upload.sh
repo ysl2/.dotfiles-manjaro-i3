08b6041353292296b56d3b877cf9e327729fb018
ysl2 <songli.yu@outlook.com>
2021-02-08 08:29:12 +0800

    2021-02-08 08:29:12

--- my-package-upload.sh
+++ my-package-upload.sh
@@ -1,6 +1,6 @@
-mkdir -p /home/yusoli/.dotfiles/.MY-PACKAGE-LIST
-mkdir -p /home/yusoli/.dotfiles/.MY-PACKAGE-LIST/i3
-cd /home/yusoli/.dotfiles/.MY-PACKAGE-LIST/i3
+mkdir -p /home/yusoli/.dotfiles-manjaro-i3/.MY-PACKAGE-LIST
+mkdir -p /home/yusoli/.dotfiles-manjaro-i3/.MY-PACKAGE-LIST/i3
+cd /home/yusoli/.dotfiles-manjaro-i3/.MY-PACKAGE-LIST/i3
 
 if [[ ! -f "my-pacman-list.txt" ]]; then
 	touch my-pacman-list.txt
@@ -12,5 +12,5 @@ sudo pacman -Qeq > my-pacman-list.txt
 yay -Qeq > my-yay-list.txt
 pip list | awk '{print $1}' > my-pip-list.txt
 
-cd /home/yusoli/.dotfiles/
-git add .MY-PACKAGE-LIST/i3/* > /dev/null && git commit -m "$(date +"%Y-%m-%d %H:%M:%S")" > /dev/null && git push origin master -q > /dev/null
+#cd /home/yusoli/.dotfiles-manjaro-i3/
+#git add .MY-PACKAGE-LIST/i3/* > /dev/null && git commit -m "$(date +"%Y-%m-%d %H:%M:%S")" > /dev/null && git push origin master -q > /dev/null
